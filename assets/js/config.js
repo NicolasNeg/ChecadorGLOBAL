0c26ad37c0573a3e85c290a836dbd9960b94a978
angel armenta <armentanegreteangelnicolas@gmail.com>
2026-06-18 03:08:32 -0600

    SE arregla conexion con supabase

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 // Reemplaza estos valores con los de tu proyecto Supabase
-export const SUPABASE_URL      = 'https://XXXXXXXXXX.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJ...TU_ANON_KEY_AQUI...';
+export const SUPABASE_URL = 'https://aintwcicbjxvyyjnypiq.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpbnR3Y2ljYmp4dnl5am55cGlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NzYwMDMsImV4cCI6MjA5NzA1MjAwM30.ehpcNmzbthjJBzlve3-fVIzFHcbMGil_wuXw1L2H_aA';
 
 export const FUNCTIONS_BASE = `${SUPABASE_URL}/functions/v1`;
